a2cbf556f7b5ad9750b59e58778491a8efe58c95
Mauricio Nunes <mauricionunesdasilvanunes@gmail.com>
2024-02-05 08:05:30 -0300

    Remove Sidebar component from billstopay.jsx

--- frontend/pages/billstopay.jsx
+++ frontend/pages/billstopay.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import Sidebar from "./components/Sidebar";
 import CreateBillToPay from "./components/financial/CreateBillToPay";
 import { DeleteIcon } from "@chakra-ui/icons";
 import io from "socket.io-client";
