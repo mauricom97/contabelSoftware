602ccc0a8cdb5cd43ca186b88290162154d07d63
Mauricio Nunes <mauricom7@hotmail.com>
2024-06-02 14:09:38 -0300

    feat: Update multerConfig to use relative path for file destination

--- backend/src/middlewares/multerConfig.ts
+++ backend/src/middlewares/multerConfig.ts
@@ -4,10 +4,7 @@ import path from "path";
 // Configuração do multer
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(
-      null,
-      "/home/mauricio/Projetos/pessoais/contabelSoftware/backend/src/files/sheets"
-    );
+    cb(null, path.resolve(__dirname, "../files/sheets"));
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
