23b7fa96a39fc4f4ea0bd98f4156495ffacc47d8
Mauricio Nunes <mauricom7@hotmail.com>
2024-06-13 22:43:31 -0300

    chore: Update RabbitMQ producer to use consistent quotation marks

--- backend/src/rabbitmq/producer.ts
+++ backend/src/rabbitmq/producer.ts
@@ -1,4 +1,4 @@
-import * as amqp from 'amqplib/callback_api';
+import * as amqp from "amqplib/callback_api";
 import path from "path";
 
 interface FileMessage {
@@ -6,36 +6,43 @@ interface FileMessage {
 }
 
 const producerRabbitMQ = async (nameFile: string) => {
-
-    amqp.connect('amqp://mauricio:123456@localhost:5672', (error0, connection) => {
-      if (error0) {
-        throw error0;
-      }
-      connection.createChannel((error1, channel) => {
-        if (error1) {
-          throw error1;
+  try {
+    amqp.connect(
+      "amqp://mauricio:123456@localhost:15672",
+      (error0, connection) => {
+        if (error0) {
+          throw error0;
         }
-        const queue = 'file_upload';
-    
-        channel.assertQueue(queue, {
-          durable: true
-        });
-    
-        const filePath = path.resolve(
-          __dirname,
-          "../../files/sheets",
-          nameFile
-        );
+        connection.createChannel((error1, channel) => {
+          if (error1) {
+            throw error1;
+          }
+          const queue = "file_upload";
 
-        const message: FileMessage = { filePath: filePath };
-    
-        channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
-          persistent: true
+          channel.assertQueue(queue, {
+            durable: true,
+          });
+
+          const filePath = path.resolve(
+            __dirname,
+            "../../files/sheets",
+            nameFile
+          );
+
+          const message: FileMessage = { filePath: filePath };
+
+          channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), {
+            persistent: true,
+          });
+
+          console.log(" [x] Sent %s", JSON.stringify(message));
         });
-    
-        console.log(" [x] Sent %s", JSON.stringify(message));
-      });
-    });
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
 
 export default producerRabbitMQ;
