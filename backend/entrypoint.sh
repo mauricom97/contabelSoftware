7d72fa1d67a8c9627bf1e5b16d8d2a45fe2b6ee2
Mauricio Nunes <mauricom7@hotmail.com>
2024-06-16 13:39:50 -0300

    chore: Update entrypoint.sh to handle different environments

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,7 @@
 if [ "$NODE_ENV" = "development" ]; then
   echo "Executando em ambiente de desenvolvimento"
   npm run dev
+else
   echo "Executando em ambiente de produção"
   npm run build && npm run start
 fi
