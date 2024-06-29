81adbc1aaea55ec8a744ff6e0e25cfe9ca8f1646
Mauricio Nunes <mauricom7@hotmail.com>
2024-06-29 08:09:38 -0300

    debug

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,11 @@
 import "express-async-errors";
 import express from "express";
 import http from "http";
-import { Server, Socket } from "socket.io";
-import cors from "cors";
-import morgan from "morgan";
-import helmet from "helmet";
-import routes from "./routes";
+// import { Server } from "socket.io";
+// import cors from "cors";
+// import morgan from "morgan";
+// import helmet from "helmet";
+// import routes from "./routes";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = parseInt(`${process.env.PORT || 4356}`);
@@ -13,32 +13,32 @@ const PORT = parseInt(`${process.env.PORT || 4356}`);
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://app-frontend-qrcodebar.onrender.com",
-      "https://contabel-software.vercel.app",
-    ], // Adicione os IPs ou domínios permitidos aqui
-    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos HTTP permitidos
-    allowedHeaders: ["Content-Type", "Authorization, token"], // Cabeçalhos permitidos
-  },
-});
+// const io = new Server(server, {
+//   cors: {
+//     origin: [
+//       "http://localhost:3000",
+//       "https://app-frontend-qrcodebar.onrender.com",
+//       "https://contabel-software.vercel.app",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type", "Authorization, token"],
+//   },
+// });
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(routes);
+// app.use(morgan("tiny"));
+// app.use(cors());
+// app.use(helmet());
+// app.use(express.json());
+// app.use(routes);
 
-io.on("connection", (socket) => {
-  console.log("A new user connected");
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
+// io.on("connection", (socket) => {
+//   console.log("A new user connected");
+//   socket.on("disconnect", () => {
+//     console.log("A user disconnected");
+//   });
+// });
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-export { io };
+// export { io };
