702fee02a4e3467d4bcf44707e5150e4f6455503
Mauricio Nunes <mauricom7@hotmail.com>
2024-07-07 00:41:25 -0300

    fix

--- backend/src/middlewares/connPrisma.ts
+++ backend/src/middlewares/connPrisma.ts
@@ -1,14 +1,13 @@
 import { PrismaClient } from "@prisma/client";
-const pgDatabase =
+const pgDatabase = !process.env.NODE_ENV ||
 process.env.NODE_ENV === "production"
 ? process.env.DATABASE_URL
 : process.env.PG_DATABASE_DEV;
-console.log(`########################${pgDatabase}##########################`)
 
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://controle_pratico_bd_user:mBQSxAbVDEd0pJ082kQhP1nEG3n1qNGr@dpg-cpvmiv6ehbks73e06na0-a/controle_pratico_bd",
+      url: pgDatabase,
     },
   },
 });
