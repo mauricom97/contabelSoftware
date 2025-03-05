e8a7421c6656842747e1da9efcd7c4a8c778506a
Mauricio Nunes <mauricom7@hotmail.com>
2025-03-05 14:01:36 -0300

    feat() add pwa

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa')({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development'
+  })
+  
+  module.exports = withPWA({
     typescript: {
-        ignoreBuildErrors: true,
+      ignoreBuildErrors: true,
     },
-};
+    // Adicione outras configurações específicas do seu projeto aqui
+  })
