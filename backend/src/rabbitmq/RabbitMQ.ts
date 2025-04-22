b919ea2c8636b88a0c3bfad3cc53d93c57ae26c4
Mauricio Nunes <mauricom7@hotmail.com>
2025-04-22 00:02:21 -0300

    fix() ajsute conexao rabbitmq

--- backend/src/rabbitmq/RabbitMQ.ts
+++ backend/src/rabbitmq/RabbitMQ.ts
@@ -1,11 +1,11 @@
-import amqp, { Channel, Connection } from "amqplib";
+import amqp, { Channel } from "amqplib";
 import dotenv from "dotenv";
 dotenv.config();
 const RABBITMQ_URL = "amqps://boraehzx:m2Oo3r5clrM2AtpRsMm1LwUnjL1fS261@shrimp.rmq.cloudamqp.com/boraehzx"
 
-export const connectRabbitMQ = async (): Promise<{ connection: Connection, channel: Channel }> => {
+export const connectRabbitMQ = async (): Promise<{ connection: any, channel: Channel }> => {
   try {
-    const connection: Connection = await amqp.connect(RABBITMQ_URL);
+    const connection = await amqp.connect(RABBITMQ_URL);
     const channel: Channel = await connection.createChannel();
     return {connection, channel};
   } catch (error) {
